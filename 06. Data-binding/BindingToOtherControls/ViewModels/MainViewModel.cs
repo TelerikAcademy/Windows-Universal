14a69555d4347e709f90056c6dc3907b09e849b2
EvlogiHr <evlogihristov@yahoo.com>
2014-11-12 16:01:33 +0200

    Removed unnecessary validations demo

---
+++ 06. Data-binding/BindingToOtherControls/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingToOtherControls.ViewModels
+{
+    public class MainViewModel
+    {
+        public Person Person { get; set; }
+
+        public MainViewModel()
+        {
+            this.Person = new Person("Ivan", 23);
+        }
+    }
+}
